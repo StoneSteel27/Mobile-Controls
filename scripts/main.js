55503084cc77620b94842eb941ed8df465e3a3cd
Kanishq <76529491+StoneSteel27@users.noreply.github.com>
2022-09-18 13:10:41 +0530

    resnapping after ending pan

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,24 @@
 const ui = require("ui-lib/library");
 
 var activated = 0;
+var orgx = 0
+var orgy = 0
 
 ui.addButton("Pan for Mobile", "copy", () => {
 	activated = !activated;
 	if (activated){
+		orgx = Core.camera.position.x
+		orgy = Core.camera.position.y
+		Vars.control.setInput(new DesktopInput());
 		Core.settings.put("keyboard",true)
 		var key = Core.keybinds.get(Binding.pan).key.ordinal()
 		Reflect.get(Core.input.getKeyboard(), "pressed").add(key)
 	} else {
-		Core.settings.put("keyboard",false)
 		var key = Core.keybinds.get(Binding.pan).key.ordinal()
 		Reflect.get(Core.input.getKeyboard(), "pressed").remove(key)
+		Core.settings.put("keyboard",false)
+		Core.camera.position.x = orgx
+		Core.camera.position.y = orgy
+		Vars.control.setInput(new MobileInput());
 	}
 });
