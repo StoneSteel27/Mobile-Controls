b118c95fc9ea40e946b283645963ab273af09cb1
Kanishq <76529491+StoneSteel27@users.noreply.github.com>
2022-09-18 11:38:16 +0530

    Fixed for working in mobile

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,11 @@ var activated = 0;
 ui.addButton("Pan for Mobile", "copy", () => {
 	activated = !activated;
 	if (activated){
+		Core.settings.put("keyboard",true)
 		var key = Core.keybinds.get(Binding.pan).key.ordinal()
 		Reflect.get(Core.input.getKeyboard(), "pressed").add(key)
 	} else {
+		Core.settings.put("keyboard",false)
 		var key = Core.keybinds.get(Binding.pan).key.ordinal()
 		Reflect.get(Core.input.getKeyboard(), "pressed").remove(key)
 	}
