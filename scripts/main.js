adb0f39a9ce39358c4d5593609523ff7c18f1cc7
MoltenSteel <76529491+StoneSteel27@users.noreply.github.com>
2024-04-06 15:17:25 +0530

    Removing ui-lib dependency

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
-const ui = require("ui-lib/library");
-
+//const ui = require("ui-lib/library");
+//print("Heyyyyyyyy");
 var activated = 0;
 var orgx = 0
 var orgy = 0
 
-ui.addButton("Pan for Mobile", "copy", () => {
-	activated = !activated;
-	if (activated){
-		Vars.ui.showInfoToast("Pan mode: [green]ON",3)
-		orgx = Core.camera.position.x
-		orgy = Core.camera.position.y
-		Vars.control.setInput(new DesktopInput());
-		Core.settings.put("keyboard",true)
-		var key = Core.keybinds.get(Binding.pan).key.ordinal()
-		Reflect.get(Core.input.getKeyboard(), "pressed").add(key)
-	} else {
-		Vars.ui.showInfoToast("Pan mode: [red]OFF",3)
-		var key = Core.keybinds.get(Binding.pan).key.ordinal()
-		Reflect.get(Core.input.getKeyboard(), "pressed").remove(key)
-		Core.settings.put("keyboard",false)
-		Core.camera.position.x = orgx
-		Core.camera.position.y = orgy
-		Vars.control.setInput(new MobileInput());
-	}
-});
+function func() {
+    activated = !activated;
+    if (activated) {
+        print("Pan Activated");
+            Vars.ui.showInfoToast("Pan mode: [green]ON", 3); orgx = Core.camera.position.x; orgy = Core.camera.position.y; Vars.control.setInput(new DesktopInput()); Core.settings.put("keyboard", true);
+            var key = Core.keybinds.get(Binding.pan).key.ordinal(); Reflect.get(Core.input.getKeyboard(), "pressed").add(key);
+        }
+        else {
+            print("Pan Deactivated");
+            Vars.ui.showInfoToast("Pan mode: [red]OFF", 3);
+            var key = Core.keybinds.get(Binding.pan).key.ordinal();
+            Reflect.get(Core.input.getKeyboard(), "pressed").remove(key);
+            Core.settings.put("keyboard", false);
+            Core.camera.position.x = orgx;
+            Core.camera.position.y = orgy;
+            Vars.control.setInput(new MobileInput());
+        }
+    }
+	
+Events.on(ClientLoadEvent, e => {Core.scene.find("waves").add(Packages.mindustry.ui.MobileButton(Vars.ui.getIcon("copy"), "Pan", func))});
